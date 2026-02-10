75394602fc81d5dd3c797e4dd9de7b9c16956e5b
juliangasal46 <juliangasal46@gmail.com>
2026-02-10 17:05:15 +0100

    Evento de click agrandar foto hecho

---
+++ Aniversario/ejecutable.js
@@ -0,0 +1,19 @@
+const overlay = document.getElementById("overlay");
+const imagenGrande = document.getElementById("imagenGrande");
+
+// Selecciona todos los cuadros
+const imagenes = document.querySelectorAll(".galeria img");
+
+imagenes.forEach(img => {
+  img.addEventListener("click", () => {
+    imagenGrande.src = img.src;
+    overlay.style.display = "flex";
+  });
+});
+
+// Cerrar al hacer click en el fondo
+overlay.addEventListener("click", () => {
+  overlay.style.display = "none";
+  imagenGrande.src = "";
+});
+
